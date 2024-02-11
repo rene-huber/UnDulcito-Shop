3e363858ccf45779a264965410daaef636e6c5fc
rene-huber <96597641+rene-huber@users.noreply.github.com>
2024-02-11 14:15:16 +0100

    setup 2

--- utils/categories.js
+++ utils/categories.js
@@ -1,12 +1,10 @@
 export const categories = [
     "All",
-    "Graphics",
-    "Fonts",
-    "Illustrations",
-    "Icons",
-    "Templates",
-    "Mockups",
-    "3D",
-    "Wallpaper",
-    "Mobile",
+    "Latter Cake",
+    "Number Cake",
+    "Shaped Cake",
+    "Cakes",
+    "Sweets",
+    "Pavlova",
+    
   ];
