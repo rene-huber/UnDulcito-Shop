857acd7d7e4ab090d8ca760a50cb972c44ca1dfc
rene-huber <96597641+rene-huber@users.noreply.github.com>
2024-02-12 12:36:17 +0100

    restore

---
+++ app/canceled/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Canceled = () => {
+  return (
+    <div className="success">
+    <h1>ooh...canceled order</h1>
+    <a href="/">Go back TO SHOPPING</a>
+  </div>
+  )
+}
+
+export default Canceled
