62a0bd791e631dbcdc5753a971c8fa971a1b711b
rene-huber <96597641+rene-huber@users.noreply.github.com>
2024-02-26 23:25:37 +0100

    cloudinary Posts img

--- app/api/work/new/route.js
+++ app/api/work/new/route.js
@@ -1,63 +1,34 @@
 import { connectToDB } from "@/mongodb/dataBase";
-import { writeFile } from "fs/promises";
-import path from "path";
 import Work from "@/models/work";
+import { NextResponse } from "next/server"
+
 
 export async function POST(req) {
   try {
-    /* Connect to MongoDB */
+
     await connectToDB();
+    const { creator, category, title, description, price, photos } = await req.json();
 
-    const data = await req.formData();
-
-    /* Extract info from the data */
-    const creator = data.get("creator");
-    const category = data.get("category");
-    const title = data.get("title");
-    const description = data.get("description");
-    const price = data.get("price");
-
-    /* Get an array of uploaded photos */
-    const photos = data.getAll("workPhotoPaths");
-
-    const workPhotoPaths = [];
-
-    /* Process and store each photo  */
-    for (const photo of photos) {
-      // Read the photo as an ArrayBuffer
-      const bytes = await photo.arrayBuffer();
-
-      // Convert it to a Buffer
-      const buffer = Buffer.from(bytes);
-
-      //for localhost:3000 this was the path for the images
-      // const workImagePath = `/Users/pro/Desktop/tienda/public/uploads/${photo.name}`
-      // const workImagePath = `/uploads/${photo.name}`;
-
-      const workImagePath = path.join("uploads", photo.name);
-
-      // Write the buffer to the filessystem
-      await writeFile(workImagePath, buffer);
-
-      // Store the file path in an array
-      workPhotoPaths.push(`/uploads/${photo.name}`);
-    }
-
-    /* Create a new Work */
+   
     const newWork = new Work({
       creator,
       category,
       title,
       description,
       price,
-      workPhotoPaths,
+      workPhotoPaths: photos
     });
 
+ 
     await newWork.save();
 
-    return new Response(JSON.stringify(newWork), { status: 200 });
+    console.log(newWork, "newWork");
+
+
+    return NextResponse.json({ message: "User registered successfully!", work: newWork }, { status: 200 });
   } catch (err) {
-    console.log(err);
+
+    console.error("Error:", err);
     return new Response("Failed to create a new Work", { status: 500 });
   }
 }
--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,17 +1,15 @@
 "use client"
-
 import React, { useState } from 'react'
 import Form from '@/components/Form'
 import "@/styles/Order.scss"
-
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 
 const CreateWork = () => {
+  const CLOUD_NAME = "huberlin";
+  const UPLOAD_PRESET = "blog13";
   const { data: session } = useSession()
-
   const router = useRouter()
-
   const [work, setWork] = useState({
     creator: "",
     category: "",
@@ -20,32 +18,65 @@ const CreateWork = () => {
     price: "",
     photos: []
   })
+  console.log( )
+  if (session) {work.creator = session?.user?._id}
 
-  if (session) {
-    work.creator = session?.user?._id
-  }
+  const uploadImage = async (photos) => {
+    if (!work.photos || work.photos.length === 0) return [];
+  
+    const uploadedImages = [];
+  
+    for (let i = 0; i < work.photos.length; i++) {
+      const formData = new FormData();
+      formData.append("file", work.photos[i]);
+      formData.append("upload_preset", UPLOAD_PRESET);
+  
+      try {
+        const res = await fetch(`https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`, {
+          method: "POST",
+          body: formData
+        });
+  
+        const data = await res.json();
+        const imageUrl = data['secure_url'];
+        uploadedImages.push(imageUrl);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  
+    return uploadedImages;
+  };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault()
 
     try {
-      const newWorkForm = new FormData()
+      const uploadedImages = await uploadImage(work.photos);
 
-      for (var key in work) {
-        newWorkForm.append(key, work[key])
-      }
-
-      work.photos.forEach((photo) => {
-        newWorkForm.append("workPhotoPaths", photo)
-      })
+        const creator = work.creator
+          const category=  work.category
+          const title=  work.title
+          const description=  work.description
+          const price=  work.price
+          const photos=  uploadedImages
+        
+  
+console.log("9879789797979797",  creator, category, title, description, price, photos)
 
       const response = await fetch(`/api/work/new`, {
         method: "POST",
-        body: newWorkForm
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ creator, category, title, description, price, photos })
       })
-
+  
       if (response.ok) {
         router.push(`/shop?id=${session?.user?._id}`)
+      } else {
+        throw new Error("Failed to create work")
       }
     } catch (err) {
       console.log("Publish Work failed", err.message)
