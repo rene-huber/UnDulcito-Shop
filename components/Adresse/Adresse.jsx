362bf316b7f1909d486c92cac23fbd70a13fde1a
rene-huber <96597641+rene-huber@users.noreply.github.com>
2024-02-27 13:45:30 +0100

    location gif

--- components/Adresse/Adresse.jsx
+++ components/Adresse/Adresse.jsx
@@ -1,12 +1,34 @@
 import "./styles.css";
+import Image from "next/image";
+
 
 function Adresse() {
   return (
-    <main className="wrap">You can order 5 days in advance at
-    Tel.+1321-7329403 and we will gladly
-    attend you.
+    <main className="wrap">
+
+      <div className="top">
+      <h2>#UnDulcito</h2>
+      <button className="button">Order Now</button>
+      </div>
+
     
-    undulcito2015@gmail.com</main>
+      The orders must be placed with 3 days advance notice
+      
+    <h3>
+      Tel.+1321-7329403
+      </h3>
+      
+    
+    <h2>undulcito2015@gmail.com</h2>
+<div className="gif" >
+<Image src='/location.gif' alt="work" width={109} height={109} className="imagen"/>
+
+<p>Delivery Service / Pickup</p>
+<h2> Now Located in Saint Cloud, Florida</h2>
+</div>
+
+    </main>
+
   )
 }
 
